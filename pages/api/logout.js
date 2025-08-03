1b12c50ea74964920e5073744c35aa9807b1744b
giran085 <klikace123@gmail.com>
2025-08-03 18:14:25 +0700

    test tma level2 fe giran

--- pages/api/logout.js
+++
@@ -1,6 +0,0 @@
-import { serialize } from 'cookie'
-
-export default function handler(req, res) {
-  res.setHeader('Set-Cookie', serialize('token', '', { path: '/', maxAge: -1 }))
-  res.status(200).json({ message: 'Logout berhasil' })
-}
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,20 +1,48 @@
 import { PrismaClient } from '@prisma/client'
-
 const prisma = new PrismaClient()
 
 async function main() {
-  await prisma.user.create({
-    data: {
-      email: 'admin@test.com',
-      password: '123456' // password ini langsung disimpan polos
-    }
+  // Tambah owners
+  await prisma.owner.createMany({
+    data: [
+      { id: 1, owner_name: 'Apple.Inc' },
+      { id: 2, owner_name: 'Samsung Ltd' }
+    ],
+    skipDuplicates: true
   })
-  console.log('User seeded!')
+
+  // Tambah products
+  await prisma.product.createMany({
+    data: [
+      { product_id: 1, product_name: 'Iphone 16 Pro', product_brand: 'phone', created_date: 'now()' },
+      { product_id: 2, product_name: 'Ipad Air 11', product_brand: 'Ipad', created_date: 'now()' },
+      { product_id: 3, product_name: 'Macbook pro 14', product_brand: 'macbook', created_date: 'now()' },
+      { product_id: 4, product_name: 'Galaxy S25 series', product_brand: 'phone', created_date: 'now()' },
+      { product_id: 5, product_name: 'Galaxy Tab S10FE', product_brand: 'tablet', created_date: 'now()' },
+      { product_id: 6, product_name: 'Evercross X8', product_brand: 'phone', created_date: 'now()' },
+      { product_id: 7, product_name: 'Advance G9', product_brand: 'phone', created_date: 'now()' },
+    ],
+    skipDuplicates: true
+  })
+
+  // Tambah relasi products_owners
+  await prisma.productOwner.createMany({
+    data: [
+      { products_id: 1, owners_id: '1' },
+      { products_id: 2, owners_id: '1' },
+      { products_id: 3, owners_id: '1' },
+      { products_id: 4, owners_id: '2' },
+      { products_id: 5, owners_id: '2' },
+    ],
+    skipDuplicates: true
+  })
+
+  console.log('✅ Data berhasil ditanam ke database.')
 }
 
 main()
-  .then(() => prisma.$disconnect())
-  .catch((e) => {
+  .catch(e => {
     console.error(e)
-    prisma.$disconnect()
+    process.exit(1)
   })
+  .finally(() => prisma.$disconnect())
